c5d12382180f13011dd8a0ed66d9c44547d03d78
skuzzy <skuzzytopten@gmail.com>
2024-02-22 12:52:58 +0630

    Standard

---
+++ assets/owl-carousel/carousel.js
@@ -0,0 +1,23 @@
+$(".owl-carousel").owlCarousel({
+  margin: 10,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 2,
+    },
+    768: {
+      items: 3,
+    },
+    1024: {
+      items: 4,
+    },
+    1440: {
+      items: 4,
+    },
+    1680: {
+      items: 5,
+    },
+  },
+});
---
+++ assets/scroll-to-top/button.js
@@ -0,0 +1,17 @@
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+}
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.querySelector(".scrollToTopBtn").style.display = "block";
+  } else {
+    document.querySelector(".scrollToTopBtn").style.display = "none";
+  }
+}
